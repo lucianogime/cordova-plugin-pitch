b3216757e8f59696d7859f163b29a57bbba538a2
AmitLT <amit@menagenet.co.il>
2015-07-11 09:48:46 +0300

    dev

--- www/audiofrequency.js
+++ www/audiofrequency.js
@@ -6,8 +6,7 @@ function handlers () {
 };
 
 var AudioFrequency = function () {
-	console.log("Init Plug");
-    // Create new event handlers on the window (returns a channel instance)
+	// Create new event handlers on the window (returns a channel instance)
     this.channels = {
         audiofrequency: cordova.addWindowEventHandler("audiofrequency")
     };
@@ -21,8 +20,7 @@ var AudioFrequency = function () {
  * Keep track of how many handlers we have so we can start and stop the native frequency listener appropriately.
  */
 AudioFrequency.onHasSubscribersChange = function () {
-	console.log("Plug start");
-  // If we just registered the first handler, make sure native listener is started.
+	// If we just registered the first handler, make sure native listener is started.
   if (this.numHandlers === 1 && handlers() === 1) {
       exec(audiofrequency._frequency, audiofrequency._error, "AudioFrequency", "start", []);
   } else if (handlers() === 0) {
@@ -37,16 +35,14 @@ AudioFrequency.onHasSubscribersChange = function () {
  */
 AudioFrequency.prototype._frequency = function (frequencyData) {
     // Fire audiofrequency event
-	console.log("fire-event");
-    cordova.fireWindowEvent("audiofrequency", frequencyData);
+	  cordova.fireWindowEvent("audiofrequency", frequencyData);
 };
 
 /**
  * Error callback for AudioFrequency start
  */
 AudioFrequency.prototype._error = function (e) {
-	console.log("error-event");
-    console.log("Error initializing AudioFrequency: " + e);
+   console.log("Error initializing AudioFrequency: " + e);
 };
 
 var audiofrequency = new AudioFrequency();
